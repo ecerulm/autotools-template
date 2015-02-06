0a60c39c32e9403b114ef2398f023cdeadfdf7be
Ruben Laguna <ruben.laguna@gmail.com>
2015-02-06 17:10:11 +0100

    Example 'make check'

---
+++ tests/test.c
@@ -0,0 +1,10 @@
+#include <glib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Write some tests!!! Failing!\n");
+  exit(EXIT_FAILURE);
+  return 0;
+}
