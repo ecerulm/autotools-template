0ffb066242cc8e80209c8e54a1d5e5e349698c89
Ruben Laguna <ruben.laguna@gmail.com>
2015-02-07 18:34:03 +0100

    Add some more testcases with assertions

--- tests/test.c
+++ tests/test.c
@@ -21,10 +21,25 @@ void my_first_test(dfixture *df, gconstpointer ignored) {
   g_assert(true);
 }
 
+void my_second_test(dfixture *df, gconstpointer ignored) {
+  g_test_fail();
+}
+
+void my_third_test(dfixture *df, gconstpointer ignored) {
+  g_test_incomplete("incomplete means that there is some functionality missing");
+}
+
+void my_fourth_test(dfixture *df, gconstpointer ignored) {
+  g_test_skip("skipped because doesn't apply to GNU/Linux systems");
+}
+
 int main(int argc, char *argv[])
 {
   /* https://developer.gnome.org/glib/stable/glib-Testing.html */
   g_test_init(&argc, &argv, NULL);
   g_test_add("/set0/my first test", /* type fixture*/ dfixture, /*tdata*/NULL, /*fsetup*/ my_setup, my_first_test, my_teardown);
+  g_test_add("/set0/my second test", /* type fixture*/ dfixture, /*tdata*/NULL, /*fsetup*/ my_setup, my_second_test, my_teardown);
+  g_test_add("/set0/my third test", /* type fixture*/ dfixture, /*tdata*/NULL, /*fsetup*/ my_setup, my_third_test, my_teardown);
+  g_test_add("/set0/my fourth test", /* type fixture*/ dfixture, /*tdata*/NULL, /*fsetup*/ my_setup, my_fourth_test, my_teardown);
   return g_test_run();
 }
