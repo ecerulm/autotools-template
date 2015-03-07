df8982b75bdd0b76568b2d71f9967acc3b026184
Ruben Laguna <ruben.laguna@gmail.com>
2015-03-07 20:41:02 +0100

    Include config.h only if HAVE_CONFIG_H

--- src/common.h
+++ src/common.h
@@ -1,5 +1,9 @@
 #ifndef COMMON_H
 #define COMMON_H
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
 void myfunc();
 
 #endif /* COMMON_H */
--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
-#include "config.h"
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
 #include <stdio.h>
 #include "common.h"
 
 int main(int argc, char *argv[])
 {
   printf ("Hello world\n");
-  myfunc();
+  printf ("PACKAGE " PACKAGE "\n");
+  printf ("PACKAGE_BUGREPORT " PACKAGE_BUGREPORT "\n");
+  printf ("PACKAGE_NAME "PACKAGE_NAME "\n");
+  printf ("PACKAGE_STRING " PACKAGE_STRING "\n");
+  printf ("PACKAGE_TARNAME " PACKAGE_TARNAME "\n");
   return 0;
 }
